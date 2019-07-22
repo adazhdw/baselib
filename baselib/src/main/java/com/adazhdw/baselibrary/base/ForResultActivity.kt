7ffeb82ae9719504354238a362eaec122209a6c9
adazhdw <dgz102486@163.com>
2019-07-22 17:46:40 +0800

    优化ListFragmentLine代码

---
+++ baselib/src/main/java/com/adazhdw/baselibrary/base/ForResultActivity.kt
@@ -0,0 +1,35 @@
+package com.adazhdw.baselibrary.base
+
+import android.content.Intent
+
+abstract class ForResultActivity : CoroutinesActivity() {
+
+    private val resultCallbackSet = mutableMapOf<Int, ((resultCode: Int, data: Intent?) -> Unit)>()
+    fun startActivityForResultCompat(
+        intent: Intent,
+        resultCallback: ((resultCode: Int, data: Intent?) -> Unit)
+    ) {
+        currentCode = requestCode()
+        resultCallbackSet[currentCode] = resultCallback
+        startActivityForResult(intent, currentCode)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == currentCode) {
+            resultCallbackSet[currentCode]?.invoke(resultCode, data)
+        }
+    }
+
+
+    private var currentCode = 0
+    private val codeSet by lazy { hashSetOf(0) }
+    private fun requestCode(): Int {
+        var code = 0
+        while (codeSet.contains(code)) {
+            code += 1
+        }
+        return code
+    }
+}
--- baselib/src/main/java/com/adazhdw/baselibrary/img/AlbumExt.kt
+++ baselib/src/main/java/com/adazhdw/baselibrary/img/AlbumExt.kt
@@ -1,8 +1,39 @@
 package com.adazhdw.baselibrary.img
 
-import androidx.fragment.app.FragmentActivity
+import android.Manifest
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri.parse
+import android.net.Uri.withAppendedPath
+import android.provider.MediaStore.ACTION_IMAGE_CAPTURE
+import android.provider.MediaStore.EXTRA_OUTPUT
+import com.adazhdw.baselibrary.base.ForResultActivity
+import com.adazhdw.baselibrary.ext.PermissionExt
+import com.adazhdw.baselibrary.ext.getImgFormat
 
 
-fun FragmentActivity.openAlbum(){
+fun ForResultActivity.openAlbum() {
+    if (!PermissionExt.isGranted(arrayOf(Manifest.permission_group.STORAGE))) {
+        PermissionExt.requestPermissions(
+            context = this,
+            permissions = arrayOf(Manifest.permission_group.STORAGE),
+            granted = {
+                val intent = Intent(ACTION_IMAGE_CAPTURE)
+                intent.putExtra(EXTRA_OUTPUT, withAppendedPath(parse(cacheDir.path), "${getImgFormat()}.png"))
+                startActivityForResultCompat(Intent(), resultCallback = { resultCode, data ->
+                    if (resultCode == RESULT_OK) {
 
+                    }
+                })
+            },
+            denied = {
+
+            })
+    } else {
+        startActivityForResultCompat(Intent(), resultCallback = { resultCode, data ->
+            if (resultCode == RESULT_OK) {
+
+            }
+        })
+    }
 }
