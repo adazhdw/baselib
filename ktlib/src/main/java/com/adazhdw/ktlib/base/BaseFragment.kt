aa9a6233dad188b56c958c90f38250885abeec71
adazhdw <dgz102486@163.com>
2019-11-20 17:40:44 +0800

    update to version 0.3.4

--- ktlib/src/main/java/com/adazhdw/ktlib/base/BaseFragment.kt
+++ ktlib/src/main/java/com/adazhdw/ktlib/base/BaseFragment.kt
@@ -6,6 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import org.greenrobot.eventbus.EventBus
 
+/**
+ * 1、布局初始化后，才能加载数据
+ * 2、界面是否可见后，才能加载数据
+ * 3、是否加载过数据，如果加载过，就不重复加载
+ */
 abstract class BaseFragment : CoroutinesFragment() {
 
     /**
@@ -33,15 +38,62 @@ abstract class BaseFragment : CoroutinesFragment() {
      */
     abstract fun requestStart()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    /**
+     * 是否初始化过布局
+     */
+    protected var isViewInitiated = false
+    /**
+     * 当前界面是否可见
+     */
+    protected var isVisibleToUser = false
+    /**
+     * 是否加载过数据
+     */
+    protected var isDataInitiated = false
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        initData()
         return inflater.inflate(layoutId, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        isViewInitiated = true
         initView(view)
-        initData()
-        requestStart()
+        prepareRequest()
+    }
+
+    /**
+     * 使用 transition 的hide 或者 show 方法会调用
+     */
+    override fun onHiddenChanged(hidden: Boolean) {
+        super.onHiddenChanged(hidden)
+        this.isVisibleToUser = hidden
+        if (this.isVisibleToUser) {
+            prepareRequest()
+        }
+    }
+
+    /**
+     * 与ViewPager 配合使用
+     */
+    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        super.setUserVisibleHint(isVisibleToUser)
+        this.isVisibleToUser = isVisibleToUser
+        if (this.isVisibleToUser) {
+            prepareRequest()
+        }
+    }
+
+    private fun prepareRequest() {
+        if (isVisibleToUser && isViewInitiated && !isDataInitiated) {
+            requestStart()
+            isDataInitiated = true
+        }
     }
 
     override fun onStart() {
