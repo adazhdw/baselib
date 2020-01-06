712e6754b7112ee4aa796e14a65ed7254e8a22f9
adazhdw <dgz102486@163.com>
2020-01-06 17:49:03 +0800

    add FastJsonTest

---
+++ app/src/main/java/com/grantgz/baseapp/http/FastJsonTest.kt
@@ -0,0 +1,33 @@
+package com.grantgz.baseapp.http
+
+import com.alibaba.fastjson.JSONObject
+import com.alibaba.fastjson.TypeReference
+import com.alibaba.fastjson.annotation.JSONType
+import com.alibaba.fastjson.parser.Feature
+import com.alibaba.fastjson.serializer.SerializerFeature
+
+
+/**
+ * Created by adazhdw on 2020/1/6.
+ */
+
+class FastJsonTest
+fun main() {
+
+    val dts = DataClassSimple(1, 2, listOf(DataClassSimple.Data2(3)))
+    val jsons = JSONObject.toJSONString(dts, SerializerFeature.WriteClassName)
+    println(jsons)
+    val clzs = DataClassSimple::class
+    println(clzs.javaObjectType)
+    val dt2 = parseObject<DataClassSimple>(jsons)
+    println("-----------------------------")
+    println(dt2)
+}
+
+inline fun <reified T : Any> parseObject(json: String): T {
+    return JSONObject.parseObject(json, object : TypeReference<T>() {})
+}
+
+data class DataClassSimple(val a: Int, val b: Int, val data: List<Data2>) {
+    data class Data2(val c: Int)
+}
