d79ba4ac0dec47a02915a1011fb3803dd56ff1a7
adazhdw <dgz102486@163.com>
2020-09-02 10:19:47 +0800

    ProgressRequestBody.java,ProgressRequestBody.java 添加

--- ktlib/src/main/java/com/adazhdw/ktlib/kthttp/callback/ProgressCallback.kt
+++ ktlib/src/main/java/com/adazhdw/ktlib/kthttp/callback/ProgressCallback.kt
@@ -8,4 +8,5 @@ package com.adazhdw.ktlib.kthttp.callback
  */
 interface ProgressCallback {
     fun updateProgress(progress: Int, networkSpeed: Long, done: Boolean)
+    fun onProgress(progress: Int, currentSize: Long, totalSize: Long)
 }
---
+++ ktlib/src/main/java/com/adazhdw/ktlib/kthttp/interceptor/ProgressInterceptor.java
@@ -0,0 +1,34 @@
+package com.adazhdw.ktlib.kthttp.interceptor;
+
+import com.adazhdw.ktlib.kthttp.body.ProgressResponseBody;
+import com.adazhdw.ktlib.kthttp.callback.ProgressCallback;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Response;
+
+/**
+ * 下载进度拦截器
+ * User: ljx
+ * Date: 2019/1/20
+ * Time: 14:19
+ */
+public class ProgressInterceptor implements Interceptor {
+
+    private ProgressCallback progressCallback;
+
+    public ProgressInterceptor(ProgressCallback progressCallback) {
+        this.progressCallback = progressCallback;
+    }
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        //拦截
+        Response originalResponse = chain.proceed(chain.request());
+        //包装响应体并返回
+        return originalResponse.newBuilder()
+                .body(new ProgressResponseBody(originalResponse, progressCallback))
+                .build();
+    }
+}
