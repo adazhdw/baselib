1ae875e964f7aa43bded810af90d69d24fc71a84
adazhdw <dgz102486@163.com>
2020-09-25 15:23:38 +0800

    SizeExt.kt 添加dp、sp转换方法

--- ktlib/src/main/java/com/adazhdw/ktlib/ext/SizeExt.kt
+++ ktlib/src/main/java/com/adazhdw/ktlib/ext/SizeExt.kt
@@ -1,6 +1,8 @@
 package com.adazhdw.ktlib.ext
 
 import android.content.Context
+import android.content.res.Resources
+import android.util.TypedValue
 import com.adazhdw.ktlib.KtLib
 
 
@@ -44,3 +46,25 @@ fun px2sp(pxValue: Float): Int {
     val fontScale = KtLib.getApp().resources.displayMetrics.scaledDensity
     return (pxValue / fontScale + 0.5f).toInt()
 }
+
+/**
+ * 在非 xml 环境下构建布局，需要将 px 转换为 dp 来进行多屏幕适配。
+ */
+val Int.dp: Int
+    get() {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            this.toFloat(),
+            Resources.getSystem().displayMetrics
+        ).toInt()
+    }
+
+val Int.sp: Int
+    get() {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_SP,
+            this.toFloat(),
+            Resources.getSystem().displayMetrics
+        ).toInt()
+    }
+
