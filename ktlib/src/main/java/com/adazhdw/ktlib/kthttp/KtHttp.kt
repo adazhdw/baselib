b5ce46573835d9ae35aade48ca4e27aa9ebd02d8
adazhdw <dgz102486@163.com>
2020-11-13 18:01:02 +0800

    优化 KtHttp

--- ktlib/src/main/java/com/adazhdw/ktlib/kthttp/KtHttp.kt
+++ ktlib/src/main/java/com/adazhdw/ktlib/kthttp/KtHttp.kt
@@ -1,10 +1,23 @@
 package com.adazhdw.ktlib.kthttp
 
+import com.adazhdw.ktlib.core.KtExecutors
 import com.adazhdw.ktlib.kthttp.callback.RequestCallback
+import com.adazhdw.ktlib.kthttp.exception.ExceptionHelper
+import com.adazhdw.ktlib.kthttp.exception.HttpStatusException
+import com.adazhdw.ktlib.kthttp.exception.NetWorkUnAvailableException
+import com.adazhdw.ktlib.kthttp.model.HttpConstant
 import com.adazhdw.ktlib.kthttp.model.Method
 import com.adazhdw.ktlib.kthttp.model.Params
 import com.adazhdw.ktlib.kthttp.request.*
+import com.adazhdw.ktlib.utils.NetworkUtils
+import okhttp3.Call
+import okhttp3.Callback
 import okhttp3.Headers
+import okhttp3.Response
+import java.io.IOException
+import java.io.InterruptedIOException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 
 /**
  * Author: dgz
@@ -195,4 +208,66 @@ class KtHttp private constructor() {
         }
         return commonParams?.params ?: mapOf()
     }
+
+    /**
+     * 网络请求具体执行处理方法
+     */
+    fun execute(requestCall: RequestCall, callback: RequestCallback?) {
+        val call = requestCall.mCall ?: return
+        callback?.onStart(call)
+        call.enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                handleResponse(response, callback)
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+                var ex: Exception = e
+                var message = e.message ?: ""
+                var code = HttpConstant.ERROR_RESPONSE_ON_FAILURE
+                if (e is SocketTimeoutException) {
+                    message = "request timeout"
+                } else if (e is InterruptedIOException && e.message == "timeout") {
+                    message = "request timeout"
+                } else if (e is UnknownHostException && !NetworkUtils.isConnected()) {
+                    message = "network unavailable"
+                    code = HttpConstant.ERROR_NETWORK_UNAVAILABLE
+                    ex = NetWorkUnAvailableException()
+                }
+                //回调跳转主线程
+                KtExecutors.mainThread.execute { handleFailure(ex, code, message, callback) }
+            }
+        })
+    }
+
+    private fun handleResponse(response: Response, callback: RequestCallback?) {
+        KtExecutors.networkIO.submit {
+            try {
+                val result = ExceptionHelper.getNotNullResult(response).string()
+                //回调跳转主线程
+                KtExecutors.mainThread.execute {
+                    callback?.onHttpResponse(response, result)
+                    callback?.onFinish()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                KtExecutors.mainThread.execute {
+                    if (e is HttpStatusException) {
+                        handleFailure(e, e.statusCode, e.message, callback)
+                    } else {
+                        handleFailure(
+                            e,
+                            HttpConstant.ERROR_RESPONSE_NORMAL_ERROR, e.message, callback
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun handleFailure(e: Exception, code: Int, msg: String?, callback: RequestCallback?) {
+        callback?.onFailure(e, code, msg)
+        callback?.onFinish()
+    }
+
 }
--- ktlib/src/main/java/com/adazhdw/ktlib/kthttp/request/RequestCall.kt
+++ ktlib/src/main/java/com/adazhdw/ktlib/kthttp/request/RequestCall.kt
@@ -1,35 +1,18 @@
 package com.adazhdw.ktlib.kthttp.request
 
-import com.adazhdw.ktlib.core.KtExecutors
+import com.adazhdw.ktlib.kthttp.KtHttp.Companion.ktHttp
 import com.adazhdw.ktlib.kthttp.callback.RequestCallback
-import com.adazhdw.ktlib.kthttp.exception.ExceptionHelper
-import com.adazhdw.ktlib.kthttp.exception.HttpStatusException
-import com.adazhdw.ktlib.kthttp.exception.NetWorkUnAvailableException
-import com.adazhdw.ktlib.kthttp.model.HttpConstant
 import com.adazhdw.ktlib.kthttp.model.KtConfig
-import com.adazhdw.ktlib.kthttp.model.Params
 import com.adazhdw.ktlib.kthttp.request.base.BaseRequest
-import com.adazhdw.ktlib.kthttp.util.OkHttpCallManager
-import com.adazhdw.ktlib.utils.NetworkUtils
 import okhttp3.Call
-import okhttp3.Callback
 import okhttp3.OkHttpClient
-import okhttp3.Response
-import java.io.IOException
-import java.io.InterruptedIOException
-import java.net.SocketTimeoutException
-import java.net.UnknownHostException
 
 /**
  * author：daguozhu
  * date-time：2020/11/13 15:28
  * description：
  **/
-class RequestCall(
-    private val baseRequest: BaseRequest,
-    private val url: String = baseRequest.url,
-    private val params: Params = baseRequest.params
-) {
+class RequestCall(private val baseRequest: BaseRequest) {
 
     private val okHttpClient: OkHttpClient = KtConfig.getOkHttpClient()
     var mCall: Call? = null
@@ -50,35 +33,8 @@ class RequestCall(
     @Suppress("UNCHECKED_CAST")
     fun execute(callback: RequestCallback?) {
         val call = getRawCall()
-        if (OkHttpCallManager.callManager.addCall(url, params, call)) {//添加成功说明没有重复请求
-            callback?.onStart(call)
-            call.enqueue(object : Callback {
-                override fun onResponse(call: Call, response: Response) {
-                    OkHttpCallManager.callManager.removeCall(url, params)
-                    handleResponse(response, callback)
-                }
-
-                override fun onFailure(call: Call, e: IOException) {
-                    OkHttpCallManager.callManager.removeCall(url, params)
-                    e.printStackTrace()
-                    var ex: Exception = e
-                    var message = e.message ?: ""
-                    var code = HttpConstant.ERROR_RESPONSE_ON_FAILURE
-                    if (e is SocketTimeoutException) {
-                        message = "request timeout"
-                    } else if (e is InterruptedIOException && e.message == "timeout") {
-                        message = "request timeout"
-                    } else if (e is UnknownHostException && !NetworkUtils.isConnected()) {
-                        message = "network unavailable"
-                        code = HttpConstant.ERROR_NETWORK_UNAVAILABLE
-                        ex = NetWorkUnAvailableException()
-                    }
-                    //回调跳转主线程
-                    KtExecutors.mainThread.execute { handleFailure(ex, code, message, callback) }
-                }
-            })
-            mCall = call
-        }
+        ktHttp.execute(this, callback)
+        mCall = call
     }
 
     /**
@@ -86,37 +42,6 @@ class RequestCall(
      */
     fun cancel() {
         mCall?.cancel()
-        OkHttpCallManager.callManager.removeCall(url, params)
-    }
-
-    private fun handleResponse(response: Response, callback: RequestCallback?) {
-        KtExecutors.networkIO.submit {
-            try {
-                val result = ExceptionHelper.getNotNullResult(response).string()
-                //回调跳转主线程
-                KtExecutors.mainThread.execute {
-                    callback?.onHttpResponse(response, result)
-                    callback?.onFinish()
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-                KtExecutors.mainThread.execute {
-                    if (e is HttpStatusException) {
-                        handleFailure(e, e.statusCode, e.message, callback)
-                    } else {
-                        handleFailure(
-                            e,
-                            HttpConstant.ERROR_RESPONSE_NORMAL_ERROR, e.message, callback
-                        )
-                    }
-                }
-            }
-        }
-    }
-
-    private fun handleFailure(e: Exception, code: Int, msg: String?, callback: RequestCallback?) {
-        callback?.onFailure(e, code, msg)
-        callback?.onFinish()
     }
 
 }
